97939ff43506e1421bba1eee403872ec490e4ad8
Алексей Заузин <lehadnk@gmail.com>
2023-04-17 15:15:42 +0300

    Removed json.json from the project

--- lib/src/main/java/adminlte/web_form/communication/validators/JsonValidator.java
+++ lib/src/main/java/adminlte/web_form/communication/validators/JsonValidator.java
@@ -1,20 +1,21 @@
 package adminlte.web_form.communication.validators;
 
 import adminlte.web_form.dto.ValidationResult;
-import org.json.*;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonFactoryBuilder;
+
+import java.io.IOException;
 
 public class JsonValidator extends AbstractFormValidator {
+    private static final JsonFactory JSON_FACTORY = new JsonFactory(new JsonFactoryBuilder().disable(JsonFactory.Feature.INTERN_FIELD_NAMES));
+
     @Override
     public ValidationResult validate(String value) {
-        try {
-            new JSONObject(value);
-        } catch (JSONException ex) {
-            // In case JSONArray is valid as well
-            try {
-                new JSONArray(value);
-            } catch (JSONException ex1) {
-                return this.errorValidationResult("Invalid JSON");
-            }
+        try(var parser = JSON_FACTORY.createParser(value)) {
+            var token = parser.nextToken();
+            while (token != null) token = parser.nextToken();
+        } catch (IOException ex) {
+            return this.errorValidationResult("Invalid JSON: " + ex.toString());
         }
 
         return this.successValidationResult();
