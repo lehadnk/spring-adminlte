37fff24ac1cf36d73e04cfc185fb110c6347e36d
Alexey Hromov <bratpidd2012@gmail.com>
2023-06-06 04:06:03 +0400

    post button option

--- lib/src/main/java/adminlte/entity_list_table/communication/http/tables/columns/ActionsColumn.java
+++ lib/src/main/java/adminlte/entity_list_table/communication/http/tables/columns/ActionsColumn.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class ActionsColumn extends AbstractColumn {
     private String templatePath = "entity_list_table/columns/actions_column.html";
     private ArrayList<ActionButton> actionButtons;
+    private ArrayList<PostActionButton> postActionButtons;
 
     public ActionsColumn(ArrayList<ActionButton> actionButtons) {
         super("actions");
@@ -21,13 +22,16 @@ public class ActionsColumn extends AbstractColumn {
 
     public Context prepareContext(Object object) {
         var ctx = new Context();
-        ctx.setVariable("buttons", this.getCellActionButtons(object));
+        ctx.setVariable("buttons", this.getCellActionButtons(object, ActionButton.class));
+        ctx.setVariable("postButtons", this.getCellActionButtons(object, PostActionButton.class));
         return ctx;
     }
 
-    private ArrayList<CellActionButton> getCellActionButtons(Object object) {
+
+    private <T> ArrayList<CellActionButton> getCellActionButtons(Object object, Class<T> buttonClass) {
         var result = new ArrayList<CellActionButton>();
         for (var actionButton : this.actionButtons) {
+            if (actionButton.getClass() != buttonClass) { continue; }
             var url = actionButton.url;
             for (var idField : actionButton.identifierFields) {
                 var identifier = this.getObjectValue(object, idField).toString();
---
+++ lib/src/main/java/adminlte/entity_list_table/communication/http/tables/columns/PostActionButton.java
@@ -0,0 +1,10 @@
+package adminlte.entity_list_table.communication.http.tables.columns;
+
+import java.util.List;
+
+public class PostActionButton extends ActionButton {
+
+    public PostActionButton(String text, String url, String identifierField, String cssClass) {
+        super(text, url, identifierField, cssClass);
+    }
+}
