043bed78e4c362d8a8106c301eeaea6c1d1aed1e
KnewOne <illia.fylypovych@apollobase.com>
2023-08-18 21:44:41 +0300

    Textarea required fix

--- lib/src/main/java/adminlte/web_form/communication/form_elements/Textarea.java
+++ lib/src/main/java/adminlte/web_form/communication/form_elements/Textarea.java
@@ -3,11 +3,13 @@ package adminlte.web_form.communication.form_elements;
 import java.util.Map;
 
 public class Textarea extends AbstractFormElement<Textarea> {
+
     private final String templatePath = "web_form/form_elements/textarea.html";
     private String textTransform = "";
     private Integer rowsCount = 10;
 
-    public Textarea() {}
+    public Textarea() {
+    }
 
     public Textarea(Integer rowsCount) {
         this();
@@ -15,9 +17,7 @@ public class Textarea extends AbstractFormElement<Textarea> {
     }
 
     @Override
-    public String getTemplatePath() {
-        return this.templatePath;
-    }
+    public String getTemplatePath() { return this.templatePath; }
 
     public Textarea setTextTransform(String textTransform) {
         this.textTransform = textTransform;
@@ -25,10 +25,11 @@ public class Textarea extends AbstractFormElement<Textarea> {
     }
 
     @Override
-    public Map<String, Object> getContextVariables() {
-        return Map.of(
-            "textTransform", textTransform,
-            "rowsCount", rowsCount
-        );
-    }
+    public Map<String, Object> getContextVariables() { return Map.of(
+        "textTransform",
+        textTransform,
+        "rowsCount",
+        rowsCount,
+        "required",
+        this.required); }
 }
