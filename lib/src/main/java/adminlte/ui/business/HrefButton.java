d2b376de52c16548781122936bb564a678737a96
Alexey <lehadnk@gmail.com>
2023-09-05 14:27:50 +0100

    Added the ability to change HrefButton class

--- lib/src/main/java/adminlte/ui/business/HrefButton.java
+++ lib/src/main/java/adminlte/ui/business/HrefButton.java
@@ -3,10 +3,17 @@ package adminlte.ui.business;
 public class HrefButton {
     public final String title;
     public final String url;
+    public String cssClass = "btn-secondary";
 
     public HrefButton(String title, String url)
     {
         this.title = title;
         this.url = url;
+    }
+
+    public HrefButton setClass(String className)
+    {
+        this.cssClass = className;
+        return this;
     }
 }
